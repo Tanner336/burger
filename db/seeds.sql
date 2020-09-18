a446c8137f19c8af05c540198cece960d6df7a7b
Tanner Cook <tannercook336@gmail.com>
2020-09-18 14:03:29 -0400

    Initial skeleton file commit

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Carolina Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', false);
